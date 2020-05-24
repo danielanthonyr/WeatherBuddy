641693240836374c5ce2bdd1bb067d7f762962d9
Daniel-Anthony Romero <danielanthonyr@gmail.com>
2020-05-23 23:13:07 -0400

    Initial Commit

---
+++ WeatherBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherBuddy
+//
+//  Created by Daniel Romero on 2020-05-23.
+//  Copyright © 2020 Daniel Romero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
